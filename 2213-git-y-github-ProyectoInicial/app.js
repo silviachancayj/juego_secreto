d6ce151069bad44bafd72b7c90e2abe61862484a
Silvia Chancay Jaramillo <silvia.chancayj@gmail.com>
2026-06-23 19:58:23 -0500

    Ajuste para 50 del número máximo

--- 2213-git-y-github-ProyectoInicial/app.js
+++ 2213-git-y-github-ProyectoInicial/app.js
@@ -1,7 +1,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
 let listaNumerosSorteados = [];
-let numeroMaximo = 100;
+let numeroMaximo = 50;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
